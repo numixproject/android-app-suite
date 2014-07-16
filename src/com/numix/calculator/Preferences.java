c5b2c18cb902eb017ee3882532dfdeb8c2c05f62
paolorotolo <paolorotolo@ubuntu.com>
2014-07-16 09:34:07 +0200

    First upload. Released version  1.0

---
+++ src/com/numix/calculator/Preferences.java
@@ -0,0 +1,69 @@
+package com.numix.calculator;
+
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+
+
+public class Preferences extends PreferenceActivity {
+
+	private static final boolean ALWAYS_SIMPLE_PREFS = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+
+
+		
+		super.onCreate(savedInstanceState);
+		setupActionBar();
+		
+	}
+
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// Show the Up button in the action bar.
+			ActionBar actionbar = getActionBar();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			// TODO: If Settings has multiple levels, Up should navigate up
+			// that hierarchy.
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+
+	}
+
+
+
+
+}
