8eb92c724738783c440badb3185e7f9f1e86f6a9
Paolo Rotolo <paolorotolo@ubuntu.com>
2014-10-27 20:00:50 +0100

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/numix/calculator_pro/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.numix.calculator_pro;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.numix.calculator_pro";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "";
+}
