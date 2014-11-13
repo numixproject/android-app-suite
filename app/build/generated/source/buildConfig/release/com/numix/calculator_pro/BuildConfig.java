af95ebd5ca6a628b8a150b7497dbe549d4b519bd
Paolo Rotolo <paolorotolo@ubuntu.com>
2014-11-13 22:34:29 +0100

    Releasing 2.6.

--- app/build/generated/source/buildConfig/release/com/numix/calculator_pro/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/numix/calculator_pro/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.numix.calculator_pro";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 4;
+  public static final int VERSION_CODE = 5;
   public static final String VERSION_NAME = "";
   /**
    * @deprecated Use {@link #APPLICATION_ID}
