1a834b2bfb50cbb6a042440bd4726142c7c01c86
Paolo Rotolo <paolorotolo@ubuntu.com>
2014-12-07 20:21:56 +0100

    * Made fab (almost) working;

--- app/src/main/java/org/numixproject/torch/MainActivity.java
+++ app/src/main/java/org/numixproject/torch/MainActivity.java
@@ -3,6 +3,7 @@ package org.numixproject.torch;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
@@ -12,9 +13,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.melnykov.fab.FloatingActionButton;
 
 
 /*
@@ -34,151 +39,103 @@ import android.widget.SeekBar;
  */
 public class MainActivity extends Activity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        try {
+            //Log.d("TORCH", "Check cam");
+            // Get CAM reference
+            cam = Camera.open();
+            camParams = cam.getParameters();
+            cam.startPreview();
+            hasCam = true;
+            //Log.d("TORCH", "HAS CAM ["+hasCam+"]");
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
         }
 
-    }
-
-        /**
-         * A placeholder fragment containing a simple view.
-         */
-    public static class PlaceholderFragment extends Fragment {
-        private Camera cam;
-        private Camera.Parameters camParams;
-        private boolean hasCam;
-        private int freq;
-        private StroboRunner sr;
-        private Thread t;
-        private boolean isChecked = false;
-
-        public PlaceholderFragment() {
-         }
-
+        // Seekbar
+        SeekBar skBar = (SeekBar) findViewById(R.id.seekBar);
+        skBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                freq = progress;
+            }
 
             @Override
-        public void onActivityCreated(Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
-            try {
-                //Log.d("TORCH", "Check cam");
-                // Get CAM reference
-                cam = Camera.open();
-                camParams = cam.getParameters();
-                cam.startPreview();
-                hasCam = true;
-                //Log.d("TORCH", "HAS CAM ["+hasCam+"]");
-            }
-            catch(Throwable t) {
-                t.printStackTrace();
+            public void onStartTrackingTouch(SeekBar seekBar) {
             }
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+        });
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-           // Let's get the reference to the toggle
-            final ImageView tBtn = (ImageView) rootView.findViewById(R.id.iconLight);
-            tBtn.setImageResource(R.drawable.off);
+    public Camera cam;
+    private Camera.Parameters camParams;
+    private boolean hasCam;
+    private int freq;
+    private StroboRunner sr;
+    private Thread t;
+    private boolean isChecked = false;
 
-            tBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    isChecked = !isChecked;
 
-                    if (isChecked)
-                        tBtn.setImageResource(R.drawable.on);
-                    else
-                        tBtn.setImageResource(R.drawable.off);
+    public void turnOn(View v) {
 
-                    turnOnOff(isChecked);
-                }
-            });
+        if (freq != 0) {
+            sr = new StroboRunner();
+            sr.freq = freq;
+            t = new Thread(sr);
+            t.start();
+            return;
+        } else
+            camParams.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+    }
 
-            // Seekbar
-            SeekBar skBar = (SeekBar) rootView.findViewById(R.id.seekBar);
-            skBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                    freq = progress;
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                }
-            });
-            return rootView;
+    private void turnOff(View v) {
+        if (t != null) {
+            sr.stopRunning = true;
+            t = null;
+            return ;
         }
-
-        private void turnOnOff(boolean on) {
-
-            if (on) {
-                if (freq != 0) {
-                    sr = new StroboRunner();
-                    sr.freq = freq;
-                    t = new Thread(sr);
-                    t.start();
-                    return ;
-                }
-                else
-                    camParams.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            }
-            if (!on) {
-               if (t != null) {
-                   sr.stopRunning = true;
-                   t = null;
-                   return ;
-               }
-                else
-                   camParams.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-            }
+        else {
+            camParams.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
 
             cam.setParameters(camParams);
             cam.startPreview();
         }
-
-
-
-        private class StroboRunner implements Runnable {
-
-            int freq;
-            boolean stopRunning = false;
-
-            @Override
-            public void run() {
-                Camera.Parameters paramsOn = PlaceholderFragment.this.cam.getParameters();
-                Camera.Parameters paramsOff = PlaceholderFragment.this.camParams;
-                paramsOn.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                paramsOff.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                try {
-                    while (!stopRunning) {
-                        PlaceholderFragment.this.cam.setParameters(paramsOn);
-                        PlaceholderFragment.this.cam.startPreview();
-                        // We make the thread sleeping
-                        Thread.sleep(100 - freq);
-                        PlaceholderFragment.this.cam.setParameters(paramsOff);
-                        PlaceholderFragment.this.cam.startPreview();
-                        Thread.sleep(freq);
-                     }
-                    }
-                catch(Throwable t) {}
-            }
-        }
-
-
     }
 
 
+    private class StroboRunner implements Runnable {
+
+        int freq;
+        boolean stopRunning = false;
+
+        @Override
+        public void run() {
+            Camera.Parameters paramsOn = cam.getParameters();
+            Camera.Parameters paramsOff = camParams;
+            paramsOn.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            paramsOff.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            try {
+                while (!stopRunning) {
+                    cam.setParameters(paramsOn);
+                    cam.startPreview();
+                    // We make the thread sleeping
+                    Thread.sleep(100 - freq);
+                    cam.setParameters(paramsOff);
+                    cam.startPreview();
+                    Thread.sleep(freq);
+                }
+            }
+            catch(Throwable t) {}
+        }
+    }
 }
+
