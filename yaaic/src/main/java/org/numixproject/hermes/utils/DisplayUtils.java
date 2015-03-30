4c2709a53769ea234e7446f5d50a5ea742443f02
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-03-30 22:45:42 +0200

    Fixed User dialog design.

--- yaaic/src/main/java/org/numixproject/hermes/utils/DisplayUtils.java
+++ yaaic/src/main/java/org/numixproject/hermes/utils/DisplayUtils.java
@@ -21,6 +21,10 @@ along with Yaaic.  If not, see <http://www.gnu.org/licenses/>.
 package org.numixproject.hermes.utils;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 /**
  * Helper class for methods regarding the display of the current device.
@@ -30,6 +34,27 @@ import android.content.Context;
 public class DisplayUtils
 {
     private static float density = -1;
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
 
     /**
      * Convert the given density-independent pixels into real pixels for the
